966ed8d02be52d71a350ed0036860ba92d148d81
Conner Smith <css289@cornell.edu>
2024-03-26 18:23:07 -0400

    Implement create_grid
    - Created array for make_matrix
    - Added valid placement check
    - Wrote function body
    - Defined place_ship
    - Added checks

--- lib/battleship.ml
+++ lib/battleship.ml
@@ -11,7 +11,7 @@ let place_ship grid (x1, y1) (x2, y2) =
   if is_valid_placement (x1, y1) (x2, y2) then begin
     for x = min x1 x2 to max x1 x2 do
       for y = min y1 y2 to max y1 y2 do
-        grid.(x).(y) <- Ship
+        grid.(y).(x) <- Ship
       done
     done;
     true
