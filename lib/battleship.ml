5e0fa1b0711442897cc14110589652b886c2d4ad
Conner Smith <css289@cornell.edu>
2024-03-26 17:47:07 -0400

    Implement place_ship
    - Defined function body
    - Wrote if statement
    - Wrote for loop
    - Adde determinant for valid placement

---
+++ lib/battleship.ml
@@ -0,0 +1,19 @@
+type cell =
+  | Empty
+  | Ship
+
+type grid = cell array array
+
+let create_grid size = Array.make_matrix size size Empty
+let is_valid_placement (x1, y1) (x2, y2) = x1 = x2 || y1 = y2
+
+let place_ship grid (x1, y1) (x2, y2) =
+  if is_valid_placement (x1, y1) (x2, y2) then begin
+    for x = min x1 x2 to max x1 x2 do
+      for y = min y1 y2 to max y1 y2 do
+        grid.(x).(y) <- Ship
+      done
+    done;
+    true
+  end
+  else false
