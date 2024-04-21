51e00921512cec23a2d0724c82f46afb872973cc
Conner Smith <css289@cornell.edu>
2024-04-21 11:55:18 -0400

    Implement place_ship
    - Added updated grid coordinates for ship placement
    - Changed Y axis to letters
    - Changed order of shoot function to y,x

--- lib/battleship.ml
+++ lib/battleship.ml
@@ -1,56 +1,46 @@
-(* open Ship *)
 type cell =
   | Empty
   | Ship
-
-(* type cell = | Empty | Ship of { size : int; hit : bool; } *)
+  | Hit
+  | Miss
 
 type grid = cell array array
 
-(* type grid = { size : int; cells : cell array array; } *)
-
 let create_grid size = Array.make_matrix size size Empty
+let char_to_index c = Char.code (Char.uppercase_ascii c) - Char.code 'A'
 
-(* let create_grid size = { size; cells = Array.make_matrix size size (Empty {
-   hit = false }) } *)
+let is_valid_placement (y1, x1) (y2, x2) =
+  (x1 = x2 && abs (y2 - y1) > 0) || (y1 = y2 && abs (x2 - x1) > 0)
 
-let is_valid_placement (x1, y1) (x2, y2) size =
-  (x1 = x2 && x1 <= size - 1) || (y1 = y2 && y1 <= size - 1)
-
-(** Check if the cell is empty *)
-let is_empty grid (y, x) = grid.(y).(x) = Empty
-
-(** Check if user's target position is empty *)
-let is_all_empty grid (x1, y1) (x2, y2) =
-  let all_empty = ref true in
-  for x = min x1 x2 to max x1 x2 do
-    for y = min y1 y2 to max y1 y2 do
-      if not (is_empty grid (y, x)) then all_empty := false
-    done
-  done;
-  !all_empty
-
-(* let place_ship grid (x1, y1) (x2, y2) size = if is_valid_placement (x1, y1)
-   (x2, y2) size then begin for x = min x1 x2 to max x1 x2 do for y = min y1 y2
-   to max y1 y2 do grid.(y).(x) <- Ship done done; true end else false *)
-
-let place_ship grid (x1, y1) (x2, y2) size =
-  if is_valid_placement (x1, y1) (x2, y2) size then
-    if is_all_empty grid (x1, y1) (x2, y2) then begin
-      for x = min x1 x2 to max x1 x2 do
-        for y = min y1 y2 to max y1 y2 do
-          grid.(y).(x) <- Ship
-        done
-      done;
+let place_ship grid (y1, x1) (y2, x2) =
+  if is_valid_placement (y1, x1) (y2, x2) then
+    let dir = if y1 = y2 then `Horizontal else `Vertical in
+    let length = max (abs (y2 - y1)) (abs (x2 - x1)) + 1 in
+    let coords =
+      List.init length (fun i ->
+          match dir with
+          | `Horizontal -> (y1, x1 + i)
+          | `Vertical -> (y1 + i, x1))
+    in
+    if List.for_all (fun (y, x) -> grid.(y).(x) = Empty) coords then begin
+      List.iter (fun (y, x) -> grid.(y).(x) <- Ship) coords;
       true
     end
-    else begin
-      Printf.printf "Error: Cannot place ship on occupied cells.\n";
-      false
-    end
-  else begin
-    Printf.printf
-      "Error: Invalid placement. Ships must be placed in a straight line \
-       within the grid bounds.\n";
-    false
-  end
+    else false
+  else false
+
+let random_placement grid num_ships max_ship_size =
+  Random.self_init ();
+  let rec place_random_ship n =
+    if n > 0 then
+      let x = Random.int 10
+      and y = Random.int 10
+      and horizontal = Random.bool ()
+      and size = Random.int max_ship_size + 1 in
+      let x2 = if horizontal then x + size - 1 else x
+      and y2 = if horizontal then y else y + size - 1 in
+      if x2 < 10 && y2 < 10 && place_ship grid (y, x) (y2, x2) then
+        place_random_ship (n - 1)
+      else place_random_ship n
+  in
+  place_random_ship num_ships
