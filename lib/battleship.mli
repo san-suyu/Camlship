4ab92b51d717eceb7014bf087aa32c99eab21f9e
Conner Smith <css289@cornell.edu>
2024-05-07 12:37:37 -0400

    Implement mli file
    - Created mli file for game logic
    - Created types and global variables
    - Added exceptions

---
+++ lib/battleship.mli
@@ -0,0 +1,18 @@
+type cell =
+  | Empty
+  | Ship of int
+  | Hit of int
+  | Miss
+
+type grid = cell array array
+
+exception InvalidPlacement
+exception InvalidInput
+
+val create_grid : int -> grid
+val print_grid : grid -> bool -> string -> unit
+val place_ship : grid -> int -> int * int -> int * int -> bool
+val shoot : grid -> int * int -> string
+val ai_guess : grid -> string
+val check_game_over : grid -> bool
+val char_to_index : char -> int
